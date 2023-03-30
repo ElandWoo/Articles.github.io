2952cfa4e0c57a6746523f768a243567756ea307
Eland <86370026+ElandWoo@users.noreply.github.com>
2023-03-30 20:37:24 +0800

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,18 +1,15 @@
-$(document).ready(function () {
-  // ...之前的思维导图代码...
+window.addEventListener('scroll', function() {
+  var backToTopButton = document.querySelector('.back-to-top');
+  if (window.pageYOffset > 200) {
+    backToTopButton.style.display = 'block';
+  } else {
+    backToTopButton.style.display = 'none';
+  }
+});
 
-  // 回到顶部按钮
-  var backToTopButton = $('#back-to-top');
-  $(window).scroll(function () {
-    if ($(this).scrollTop() > 100) {
-      backToTopButton.fadeIn();
-    } else {
-      backToTopButton.fadeOut();
-    }
-  });
-
-  backToTopButton.click(function () {
-    $('html, body').animate({scrollTop: 0}, 800);
-    return false;
+document.querySelector('.back-to-top').addEventListener('click', function() {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
   });
 });
