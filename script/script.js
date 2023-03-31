66947d4abe62d3dd8ba0ea670ab1f2a2dcabed31
Eland <86370026+ElandWoo@users.noreply.github.com>
2023-03-31 15:15:32 +0800

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,1 +1,25 @@
-window.addEventListener("scroll", function () { var backToTopButton = document.querySelector(".back-to-top-btn"); if (window.pageYOffset > 200) { backToTopButton.style.display = "block" } else { backToTopButton.style.display = "none" } }); document.querySelector(".back-to-top-btn").addEventListener("click", function () { window.scrollTo({ top: 0, behavior: "smooth" }) });
+const backToTopButton = document.querySelector('.back-to-top-btn');
+
+window.addEventListener('scroll', () => {
+  const scrollHeight = document.documentElement.scrollHeight;
+  const scrollTop = document.documentElement.scrollTop;
+  const clientHeight = document.documentElement.clientHeight;
+  const scrollPercentage = (scrollTop + clientHeight) / scrollHeight * 100;
+
+  // Update the scroll percentage shown on the button
+  backToTopButton.querySelector('.back-to-top-percent').textContent = ' ${ Math.round(scrollPercentage) }%';
+
+  // If the user has scrolled past 20% of the page height, show the button
+  if (scrollPercentage > 20) {
+    backToTopButton.style.display = 'flex';
+  } else {
+    backToTopButton.style.display = 'none';
+  }
+});
+
+backToTopButton.addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+});
