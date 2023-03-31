08f47ee6c2c298695e6954622ad3f9951a8722b5
Eland <86370026+ElandWoo@users.noreply.github.com>
2023-03-31 15:04:30 +0800

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,18 +1,1 @@
-// JavaScript代码
-const backToTopButton = document.querySelector('.back-to-top-btn');
-
-window.addEventListener('scroll', () => {
-  if (window.pageYOffset > 100) {
-    backToTopButton.classList.add('show');
-  } else {
-    backToTopButton.classList.remove('show');
-  }
-});
-
-backToTopButton.addEventListener('click', () => {
-  window.scrollTo({
-    top: 0,
-    behavior: 'smooth'
-  });
-});
-
+window.addEventListener("scroll",function(){var backToTopButton=document.querySelector(".back-to-top");if(window.pageYOffset>200){backToTopButton.style.display="block"}else{backToTopButton.style.display="none"}});document.querySelector(".back-to-top").addEventListener("click",function(){window.scrollTo({top:0,behavior:"smooth"})});
