9317d671b8cc52ce84b39b9d4c173b3c5d8c4a2c
Eland <86370026+ElandWoo@users.noreply.github.com>
2023-04-13 11:51:44 +0800

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,49 +1,102 @@
-
-$(document).ready(function () {
-    // 回到顶部按钮
-    var backToTopButton = $('#back-to-top-btn');
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 100) {
-            backToTopButton.fadeIn();
-        } else {
-            backToTopButton.fadeOut();
-        }
+// 爱心特效
+(function (window, document, undefined) {
+  var hearts = [];
+  window.requestAnimationFrame = (function () {
+    return window.requestAnimationFrame ||
+      window.webkitRequestAnimationFrame ||
+      window.mozRequestAnimationFrame ||
+      window.oRequestAnimationFrame ||
+      window.msRequestAnimationFrame ||
+      function (callback) {
+        setTimeout(callback, 1000 / 60);
+      }
+  })();
+  init();
+  function init() {
+    css(".heart{width: 10px;height: 10px;position: fixed;background: #f00;transform: rotate(45deg);-webkit-transform: rotate(45deg);-moz-transform: rotate(45deg);}.heart:after,.heart:before{content: '';width: inherit;height: inherit;background: inherit;border-radius: 50%;-webkit-border-radius: 50%;-moz-border-radius: 50%;position: absolute;}.heart:after{top: -5px;}.heart:before{left: -5px;}");
+    attachEvent();
+    gameloop();
+  }
+  function gameloop() {
+    for (var i = 0; i < hearts.length; i++) {
+      if (hearts[i].alpha <= 0) {
+        document.body.removeChild(hearts[i].el);
+        hearts.splice(i, 1);
+        continue;
+      }
+      hearts[i].y--;
+      hearts[i].scale += 0.004;
+      hearts[i].alpha -= 0.013;
+      hearts[i].el.style.cssText = "left:" + hearts[i].x + "px;top:" + hearts[i].y + "px;opacity:" + hearts[i].alpha + ";transform:scale(" + hearts[i].scale + "," + hearts[i].scale + ") rotate(45deg);background:" + hearts[i].color;
+    }
+    requestAnimationFrame(gameloop);
+  }
+  function attachEvent() {
+    var old = typeof window.onclick === "function" && window.onclick;
+    window.onclick = function (event) {
+      old && old();
+      createHeart(event);
+    }
+  }
+  function createHeart(event) {
+    var d = document.createElement("div");
+    d.className = "heart";
+    hearts.push({
+      el: d,
+      x: event.clientX - 5,
+      y: event.clientY - 5,
+      scale: 1,
+      alpha: 1,
+      color: randomColor()
     });
+    document.body.appendChild(d);
+  }
+  function css(css) {
+    var style = document.createElement("style");
+    style.type = "text/css";
+    try {
+      style.appendChild(document.createTextNode(css));
+    } catch (ex) {
+      style.styleSheet.cssText = css;
+    }
+    document.getElementsByTagName('head')[0].appendChild(style);
+  }
+  function randomColor() {
+    return "rgb(" + (~~(Math.random() * 255)) + "," + (~~(Math.random() * 255)) + "," + (~~(Math.random() * 255)) + ")";
+  }
+})(window, document);
 
-    backToTopButton.click(function () {
-        $('html, body').animate({ scrollTop: 0 }, 800);
-        return false;
-    });
-
+// 手机端导航栏按钮点击事件
+var navbarToggle = document.querySelector('.navbar-toggle');
+var sidebar = document.querySelector('.sidebar');
+var sidebarOverlay = document.querySelector('.sidebar-overlay');
+navbarToggle.addEventListener('click', function () {
+  sidebar.classList.toggle('show');
+  sidebarOverlay.classList.toggle('show');
 });
 
+const backToTopButton = document.querySelector('.back-to-top-btn');
 
+window.addEventListener('scroll', () => {
+  const scrollHeight = document.documentElement.scrollHeight;
+  const scrollTop = document.documentElement.scrollTop;
+  const clientHeight = document.documentElement.clientHeight;
+  const scrollPercentage = (scrollTop + clientHeight) / scrollHeight * 100;
 
-// const backToTopBtn = document.querySelector('.back-to-top-btn');
-// const percentSpan = backToTopBtn.querySelector('.back-to-top-percent');
+  // Update the scroll percentage shown on the button
+  backToTopButton.querySelector('.back-to-top-percent').textContent = ' ${ Math.round(scrollPercentage) }%';
 
-// // Hide the button when the page is at the top
-// window.addEventListener('scroll', () => {
-//   const scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
-//   if (scrollTop === 0) {
-//     backToTopBtn.style.display = 'none';
-//   } else {
-//     backToTopBtn.style.display = 'block';
-//   }
-// });
+  // If the user has scrolled past 20% of the page height, show the button
+  if (scrollPercentage > 20) {
+    backToTopButton.style.display = 'flex';
+  } else {
+    backToTopButton.style.display = 'none';
+  }
+});
 
-// // Update the percentage displayed in the span element as the user scrolls
-// window.addEventListener('scroll', () => {
-//   const scrollTop = document.documentElement.scrollTop  document.body.scrollTop;
-//   const scrollHeight = document.documentElement.scrollHeight  document.body.scrollHeight;
-//   const percentScrolled = (scrollTop / (scrollHeight - window.innerHeight)) * 100;
-//   percentSpan.textContent = ${Math.round(percentScrolled)}%;
-// });
-
-// // Scroll to the top of the page when the button is clicked
-// backToTopBtn.addEventListener('click', () => {
-//   window.scrollTo({
-//     top: 0,
-//     behavior: 'smooth'
-//   });
-// });
+backToTopButton.addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+});
